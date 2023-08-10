cfa0bf275b51bb12110701ca34d7811f4520c355
beror <beror>
2023-08-10 17:50:15 +0300

    refactor: refactor `clean_me.js`

--- web/pages/clean_me.js
+++ web/pages/clean_me.js
@@ -1,42 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useRouter } from 'next/router';
 
 export default function MessyPage() {
   const router = useRouter();
-  const [data, setData] = React.useState(null);
-  const [loading, setLoading] = React.useState(false);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  React.useEffect(() => {
-    async function getData() {
-      setLoading(true);
-      //fake an api request and wait 5 second
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const data = await response.json();
-      setData(data);
-      setLoading(false);
-    }
-      getData();
+  useEffect(() => {
+    getData()
+        .catch((error) => setError(error));
   }, []);
+
+  async function getData() {
+    try {
+        setIsLoading(true);
+        const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+        setData(data);
+    } catch (error) {
+        //We can custom our error here
+        throw new Error('Something going wrong')
+    } finally {
+        setIsLoading(false);
+    }
+  }
+
+  if(error) return <p>error.message</p>
+
+  if(isLoading) return <p>Loading...</p>;
+
+  if(!data || data.length === 0) return <p>Empty list</p>
+
+  const renderItem = (item) => (
+      <div key={item.id} style={{ margin: '10px 0', padding: '10px', border: '1px solid black' }}>
+        <h2 style={{ fontSize: '25px' }}>{item.title}</h2>
+        <p>{item.description}</p>
+        <button
+            onClick={() => router.push(`/detail/${item.id}`)}
+            style={{ padding: '10px', backgroundColor: 'green', color: 'white', border: 'none', cursor: 'pointer' }}
+        >
+          View Detail
+        </button>
+      </div>
+  );
 
   return (
     <div style={{ fontFamily: 'Arial', color: 'black', backgroundColor: 'white', padding: '20px' }}>
       <h1 style={{ fontSize: '30px', color: 'blue' }}>Page Title</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        data && data?.map((item, i) => (
-          <div key={i} style={{ margin: '10px 0', padding: '10px', border: '1px solid black' }}>
-            <h2 style={{ fontSize: '25px' }}>{item.title}</h2>
-            <p>{item.description}</p>
-            <button
-              onClick={() => router.push(`/detail/${item.id}`)}
-              style={{ padding: '10px', backgroundColor: 'green', color: 'white', border: 'none', cursor: 'pointer' }}
-            >
-              View Detail
-            </button>
-          </div>
-        ))
-      )}
+      {data.map(renderItem)}
     </div>
   );
 }
