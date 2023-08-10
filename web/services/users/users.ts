fa36c9c364590d111521902100a98abaa40395c0
beror <beror>
2023-08-10 17:42:18 +0300

    feat: setup store and endpoints

---
+++ web/services/users/users.ts
@@ -0,0 +1,32 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import {ICreateUser, IUser} from "@/types/users";
+
+export const usersApi = createApi({
+    reducerPath: 'usersApi',
+    tagTypes: ['User'],
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
+    endpoints: (builder) => ({
+
+        getListOfUsers: builder.query<IUser[], undefined>({
+            query: () => `users`,
+            providesTags: ['User'],
+        }),
+
+        getUserById: builder.query<IUser, number>({
+            query: (id) => `users/${id}`,
+        }),
+
+        createUser: builder.mutation<IUser, ICreateUser>({
+            query(body) {
+                return {
+                    url: `users`,
+                    method: 'POST',
+                    body,
+                }
+            },
+            invalidatesTags: ['User'],
+        })
+    }),
+})
+
+export const { useGetListOfUsersQuery, useGetUserByIdQuery, useCreateUserMutation } = usersApi;
---
+++ web/types/users.ts
@@ -0,0 +1,12 @@
+export interface IUser{
+    id: number;
+    name: string;
+    email: string;
+    avatar: string;
+}
+
+export interface ICreateUser{
+    name: string;
+    email: string;
+    avatar: string;
+}
